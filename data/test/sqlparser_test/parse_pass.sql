5e2ee25c4c5547939a8470303049d4a0c08db98f
shlomi-noach <shlomi@openark.org>
2015-01-03 17:01:09 +0100

    Finalized support for IS [NOT] (TRUE|FALSE), plus tests

--- data/test/sqlparser_test/parse_pass.sql
+++ data/test/sqlparser_test/parse_pass.sql
@@ -73,6 +73,9 @@ select /* not between */ 1 from t where a not between b and c
 select /* is null */ 1 from t where a is null
 select /* is not null */ 1 from t where a is not null
 select /* is true */ 1 from t where a is true
+select /* is not true */ 1 from t where a is not true
+select /* is false */ 1 from t where a is false
+select /* is not false */ 1 from t where a is not false
 select /* < */ 1 from t where a < b
 select /* <= */ 1 from t where a <= b
 select /* >= */ 1 from t where a >= b
